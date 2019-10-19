cc19687b68b4b936c02f07706d76e1d8d50ba68a
Dan Pashchenko <bogdan.pashchenko@gmail.com>
2019-10-19 03:35:56 +0300

    Adds VideoTrimView in storyboard

--- VideoEditControl/UIView+roundCorners.swift
+++ VideoEditControl/UIView+roundCorners.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 extension UIView {
    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
         let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
